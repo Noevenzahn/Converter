021dd98fa67b3bc6cbfa13e1d6fa5966c3874634
Noevenzahn <info.artuofficial@gmail.com>
2021-08-08 14:42:05 +0200

    Initial Commit

---
+++ Converter°/Converter_App.swift
@@ -0,0 +1,17 @@
+//
+//  Converter_App.swift
+//  Converter°
+//
+//  Created by Melvin Waberski on 08.08.21.
+//
+
+import SwiftUI
+
+@main
+struct Converter_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
